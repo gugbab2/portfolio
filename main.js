94a0e36fd29cfd5e7dc46e2c034ac9c32d243aec
gugbab2 <jkhoo1997@gmail.com>
2022-05-14 15:22:11 +0900

    JS: menu scroll event

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 'use scrict';
-
+// Navbar 를 스크롤에 따라서 투명하게 만들기!
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-    
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+//Navbar 스크롤 이벤트
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
 });
